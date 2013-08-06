90ddf8d0adefb7ef03ef8a202a32e7f18948e8d2
Jeremie Dimino <jdimino@janestreet.com>
2013-08-06 10:33:21 -0400

    109.37.00

--- lib/import.ml
+++ lib/import.ml
@@ -15,6 +15,4 @@ let concat = String.concat
 let eprints = Core.Std.Debug.eprints
 let eprint  = Core.Std.Debug.eprint
 
-module Poly = Polymorphic_compare
-
 let _squelch_unused_module_warning_ = ()
