f13247c29c97ff0f9f1a2af84ae01ec0e543f614
Marek Kubica <marek@xivilization.net>
2018-08-31 16:02:04 +0200

    Implement Deferred.Result.fail
    Signed-off-by: Marek Kubica <marek@xivilization.net>

--- src/deferred_result.ml
+++ src/deferred_result.ml
@@ -31,4 +31,7 @@ include Monad.Make2 (struct
 
 let ignore = ignore_m
 
+let fail x = Deferred.return (Error x)
+let failf format = Printf.ksprintf fail format
+
 let map_error t ~f = Deferred.map t ~f:(fun r -> Result.map_error r ~f)
