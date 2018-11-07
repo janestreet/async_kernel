54458bbcceaf209da9f0e0b6c9e1df43a59d83c8
Corey Richardson <cmr@o1labs.org>
2018-11-06 17:54:09 -0800

    add trace_new_thread

--- src/tracing.ml
+++ src/tracing.ml
@@ -1,4 +1,6 @@
 type tracing_fns = 
-{ trace_thread_switch : Execution_context.t -> unit }
+{ trace_thread_switch : Execution_context.t -> unit 
+; trace_new_thread : string -> Execution_context.t -> unit
+}
 
-let fns = ref { trace_thread_switch= fun _ -> () }
+let fns = ref { trace_thread_switch= (fun _ -> ()); trace_new_thread= (fun _ _ -> ()) }
