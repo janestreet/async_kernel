733c46e1712fb8b01cc3d7e5078b188b11e65e42
Corey Richardson <cmr@o1labs.org>
2019-02-14 14:52:26 -0800

    fix tracing around cycle boundaries
    previously we would log a threadswitch whenever the current tid changes,
    or if the cycle just started and the previous job tid isn't zero.
    
    now we properly log a threadswitch if the _next_ job tid isn't zero.
    we only set cycle_started to false if we run a non-tid0 task.
    
    we still log on every tidswitch boundary if (not cycle_started).

--- src/scheduler0.ml
+++ src/scheduler0.ml
@@ -7,9 +7,12 @@ let events t = t.time_source.events
 let set_execution_context t execution_context =
   (* Avoid a caml_modify in most cases. *)
   (* XXX: see where job_queue also modifies current_execution_context *)
-  if not (phys_equal t.current_execution_context execution_context)
-  then (
-    if t.current_execution_context.tid <> execution_context.tid then (!Tracing.fns).trace_thread_switch execution_context 
-    else if t.cycle_started && t.current_execution_context.tid <> 0 then ((!Tracing.fns).trace_thread_switch execution_context; t.cycle_started <- false) ;
+  if not (phys_equal t.current_execution_context execution_context) then (
+    if t.cycle_started && execution_context.tid <> 0 then (
+      !Tracing.fns.trace_thread_switch execution_context ;
+      t.cycle_started <- false )
+    else if
+      (not t.cycle_started)
+      && t.current_execution_context.tid <> execution_context.tid
+    then !Tracing.fns.trace_thread_switch execution_context ;
     t.current_execution_context <- execution_context )
-;;
